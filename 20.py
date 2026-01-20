afccad9828fb9dc78a544bf61aa81698f2ca69e8
Nuray <n_otangazhy@kbtu.kz>
2026-01-21 02:04:04 +0500

    первая лабка

---
+++ 20.py
@@ -0,0 +1,8 @@
+x = int(input())
+y = int(input())
+if x > y:
+    print(x)
+elif x < y:
+    print(y)
+elif x == y:
+    print("equal")
---
+++ 3.py
@@ -0,0 +1,5 @@
+name = input()
+if name.isdigit():
+    print("int")
+else:
+    print("str")
