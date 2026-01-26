6ebb41bd7c4ff1d3f0fe9890d0abde2f8d563f0b
Nuray <n_otangazhy@kbtu.kz>
2026-01-26 09:06:02 +0500

    Add practice1 python examples

---
+++ p1/Practice1/python-basics/syntax.py
@@ -0,0 +1,25 @@
+if 5 > 2:
+    print("Five is grearter than two!")
+
+if 5 > 2:
+ print("Five is grearter than two!")
+if 5 > 2:
+         print("Five is grearter than two!")
+
+x = 5
+y = "Hello, World!"
+
+#This is a comment.
+print("Hello, World!")
+
+
+
+#Statments
+
+print("Python is fun!")
+
+print("Hello World!")
+print("Have a good day.")
+print("Learning Python is fun!")
+
+print("Hello"); print("How are you?"); print("Bye bye!")
